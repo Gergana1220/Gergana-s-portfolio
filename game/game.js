ac7fe8df087c3a989d1610e8b2efe1a2e6e7bd91
BoyanStoilov123 <et.angel.s@eduiteh.eu>
2026-03-13 09:40:08 +0200

    novo kym igrata

--- game/game.js
+++ game/game.js
@@ -1,95 +1,177 @@
 const canvas = document.getElementById("game-canvas");
 const context = canvas.getContext("2d");
 
-const playerWidth = 70;
+const playerWidth  = 70;
 const playerHeight = 100;
+
+const enemyWidth  = 60;
+const enemyHeight = 70;
+
+const targetFPS = 60;
 
 let timeLast = 0;
 
 let playerX = 400;
 let playerY = 300;
-let playerSpeed = 5;
+let playerSpeed = 125;
+
+let enemyX = 100;
+let enemyY = 100;
+let enemySpeed = 125;
+
+let bulletX = 400;
+let bulletY = 300;
+let bulletSpeed = 500;
+
+let bulletRequired = false;
 
 let buttonADown = false;
 let buttonDDown = false;
 let buttonWDown = false;
 let buttonSDown = false;
+let buttonSpaceDown = false;
+
+class Entity
+{
+    constructor(type, x, y, w, h, speed)
+    {
+        this.type   = type;
+        this.x      = x;
+        this.y      = y;
+        this.height = h;
+        this.width  = w;
+        this.speed  = speed;
+    }
+
+    move(dX, dY)
+    {
+        this.x += dX;
+        this.y += dY;
+    }
+}
+
+const player = new Entity("player", 400, 300, 70, 100, 125);
 
 addEventListener("keydown", (event) => {
-  if (event.key === "a")  
-    buttonADown = true; 
+    if (event.key === "a")
+        buttonADown = true;
 
-  if (event.key === "d")
-    buttonDDown = true; 
+    if (event.key === "d")
+        buttonDDown = true;
 
-  if (event.key === "w")
-    buttonWDown = true;
+    if (event.key === "w")
+        buttonWDown = true;
 
-  if (event.key === "s")
-    buttonSDown = true;
+    if (event.key === "s")
+        buttonSDown = true;
+
+    if (event.key === " ")
+        buttonSpaceDown = true;
 });
 
- addEventListener("keyup", (event) => {
-  if (event.key === "a")
-    buttonADown = false; 
+addEventListener("keyup", (event) => {
+    if (event.key === "a")
+        buttonADown = false;
 
-  if (event.key === "d")
-    buttonDDown = false; 
+    if (event.key === "d")
+        buttonDDown = false;
 
-  if (event.key === "w")
-    buttonWDown = false;
+    if (event.key === "w")
+        buttonWDown = false;
 
-  if (event.key === "s")
-    buttonSDown = false;
+    if (event.key === "s")
+        buttonSDown = false;
+
+    if (event.key === " ")
+        buttonSpaceDown = false;
 });
 
 function update(timeCurrent)
 {
-    const deltaTime = timeCurrent - timeLast;
-    console.log(deltaTime);
+    const deltaTime = (timeCurrent - timeLast) / 1000;
+    const singleFrameTime = (1000 / targetFPS) / 1000;
+
+    // console.log(`${deltaTime} < ${singleFrameTime}`);
+
+    if (deltaTime < singleFrameTime)
+    {
+        requestAnimationFrame(update);
+        return;
+    }
 
     if (buttonADown)
-        playerX -= playerSpeed; 
-    
+        player.move(-player.speed * deltaTime, 0);
+
     if (buttonDDown)
-        playerX += playerSpeed; 
+        player.move(player.speed * deltaTime, 0);
 
     if (buttonWDown)
-        playerY -= playerSpeed; 
-    
+        player.move(0, -player.speed * deltaTime);
+
     if (buttonSDown)
-        playerY += playerSpeed;
-    
-    if ((playerX + (playerWidth/2)) >= canvas.width)
+        player.move(0, player.speed * deltaTime);
+
+    if (buttonSpaceDown)
     {
-    playerX = canvas.width - (playerWidth / 2);
+        bulletRequired = true;
+        bulletX = player.x;
+        bulletY = player.y
     }
 
-    if ((playerX - (playerWidth/2)) < 0)
+    if ((player.x + (player.width / 2)) >= canvas.width)
     {
-    playerX = playerWidth / 2;
+        player.x = canvas.width - (player.width / 2);
     }
 
-    if ((playerY + (playerHeight/2)) >= canvas.height)
+    if ((player.x - (player.width / 2)) < 0)
     {
-    playerY = canvas.height - playerHeight / 2;
+        player.x = player.width / 2;
     }
 
-    if ((playerY - (playerHeight/2))  < 0)
+    if ((player.y + (player.height / 2)) >= canvas.height)
     {
-    playerY = playerHeight/2;
+        player.y = canvas.height - (player.height / 2);
     }
 
-context.clearRect(0, 0, canvas.width, canvas.height);
-context.fillStyle = "#2A2C24";
-context.fillRect(
-    playerX - (playerWidth / 2),
-    playerY - (playerHeight / 2),
-    playerWidth,
-    playerHeight); 
+    if ((player.y - (player.height / 2)) < 0)
+    {
+        player.y = player.height / 2;
+    }
 
-    timeLast = timeCurrent; 
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    if (bulletRequired)
+    {
+        bulletX -= bulletSpeed * deltaTime;
+
+        context.fillStyle = "black";
+        const bullet = new Path2D();
+        bullet.arc(
+            bulletX,
+            bulletY,
+            15,
+            0,
+            Math.PI * 2);
+        context.fill(bullet);
+    }
+
+    context.fillStyle = "red";
+    context.fillRect(
+        enemyX - (enemyWidth  / 2),
+        enemyY - (enemyHeight / 2),
+        enemyWidth,
+        enemyHeight);
+
+    context.fillStyle = "#2A2C24";
+    context.fillRect(
+        player.x - (player.width  / 2),
+        player.y - (player.height / 2),
+        player.width,
+        player.height);
+
+    timeLast = timeCurrent;
+
+    requestAnimationFrame(update);
+}
 
 requestAnimationFrame(update);
-}
-update();
